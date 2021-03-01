3a2f4756a604ed151ceb2de2b19ffc0cae42dd8e
mars <13673180154@163.com>
2021-03-01 23:11:58 +0800

    JNDI

---
+++ projects/stage-0/user-platform/user-web/src/main/java/org/geektimes/projects/user/web/controller/RegisterController.java
@@ -0,0 +1,43 @@
+package org.geektimes.projects.user.web.controller;
+
+import org.geektimes.projects.user.domain.User;
+import org.geektimes.projects.user.sql.DBConnectionManager;
+import org.geektimes.web.mvc.controller.PageController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import java.beans.BeanInfo;
+import java.beans.Introspector;
+import java.beans.PropertyDescriptor;
+import java.sql.Connection;
+import java.sql.Statement;
+import java.util.Enumeration;
+import java.util.logging.Logger;
+
+@Path("/register")
+public class RegisterController implements PageController {
+
+    private final static Logger logger = Logger.getLogger(RegisterController.class.getName());
+
+    @Path("/user")
+    @POST
+    public String execute(HttpServletRequest request, HttpServletResponse response) throws Throwable {
+        DBConnectionManager dbConnectionManager = (DBConnectionManager) request.getServletContext().getAttribute("dbConnectionManager");
+       Connection connection =  dbConnectionManager.getConnection();
+        Statement statement = connection.createStatement();
+        String sql=  " INSERT INTO users(name,password,email,phoneNumber) VALUES ";
+        BeanInfo userBeanInfo = Introspector.getBeanInfo(User.class, Object.class);
+        Enumeration<String> attrs = request.getAttributeNames();
+        while (attrs.hasMoreElements()){
+            for (PropertyDescriptor propertyDescriptor : userBeanInfo.getPropertyDescriptors()) {
+                 String name =  propertyDescriptor.getName();
+                logger.info("filed name i "+name);
+            }
+        }
+        StringBuffer sb = new StringBuffer(sql);
+        statement.execute(sb.toString());
+        return "success.jsp";
+    }
+}
--- projects/stage-0/user-platform/user-web/src/main/java/org/geektimes/projects/user/web/listener/DBConnectionInitializerListener.java
+++ projects/stage-0/user-platform/user-web/src/main/java/org/geektimes/projects/user/web/listener/DBConnectionInitializerListener.java
@@ -1,19 +1,53 @@
 package org.geektimes.projects.user.web.listener;
 
+import org.geektimes.projects.user.sql.DBConnectionManager;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.logging.Logger;
 
 @WebListener
 public class DBConnectionInitializerListener implements ServletContextListener {
 
+    private Logger logger  = Logger.getLogger(DBConnectionInitializerListener.class.getName());
+
     @Override
     public void contextInitialized(ServletContextEvent sce) {
-
+        logger.info("DBConnectionInitializerListener contextInitialized");
+        cache(sce.getServletContext());
     }
 
     @Override
     public void contextDestroyed(ServletContextEvent sce) {
+        DBConnectionManager  dbConnectionManager = (DBConnectionManager) sce.getServletContext().getAttribute("dbConnectionManager");
+        Connection connection =  dbConnectionManager.getConnection();
+        try {
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 
+    private void cache(ServletContext context){
+        // 获取JNDI数据源
+        try {
+            Context initCtx = new InitialContext();
+            Context envCtx = (Context) initCtx.lookup("java:comp/env");
+            // Look up our data source
+            DataSource ds = (DataSource) envCtx.lookup("jdbc/UserPlatformDB");
+            Connection connection =  ds.getConnection("","");
+            DBConnectionManager dbConnectionManager = new DBConnectionManager();
+            dbConnectionManager.setConnection(connection);
+            context.setAttribute("dbConnectionManager",dbConnectionManager);
+        } catch (Exception e) {
+           logger.info("DBConnectionInitializerListener Exception:"+e.getCause());
+        }
     }
 }
